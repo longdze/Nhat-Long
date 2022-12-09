3697a35c5b2ea04efcb3b1abdfd288807344a8a8
longdze <115168657+longdze@users.noreply.github.com>
2022-12-09 10:41:19 +0700

    Delete taptinvathumuc02 directory

--- taptinvathumuc02/bai2.py
+++
@@ -1,6 +0,0 @@
-def doc_tap_tin_bai2():
-    nguon=input("Nhap ten tap tin(nguồn):")
-    with open(nguon,"r") as f:
-        doc=f.read()
-        print(doc)
-doc_tap_tin_b2()
--- taptinvathumuc02/bài4.py
+++
@@ -1,3 +0,0 @@
-#đọc tập tin bài 3 và in ra màn hình
-import bai2
-bai2.doc_tap_tin_bai2()
